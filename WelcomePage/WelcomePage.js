4c183af83179b88492faad86352d88b4adfe2f01
EmanHass <eman_hassouna@hotmail.com>
2021-08-05 20:37:02 +0300

    Add function 'welcome' contain condition to store userName in localStorage relates #26

--- WelcomePage/WelcomePage.js
+++ WelcomePage/WelcomePage.js
@@ -1,2 +1,15 @@
 const inpName = document.getElementsByClassName('inpName')[0];
 const btnLogin = document.getElementsByClassName('btnLogin')[0];
+
+btnLogin.onclik = welcome;
+
+function welcome(e){
+    if(inpName.value == '') 
+        alert('Enter Your Name Please');
+    else{
+        e.preventDefault();
+        localStorage.setItem('userName',inpName.value);
+        window.location.href = "../MainPage/MainPage.html";
+        inpName.value='';       
+    }
+}
