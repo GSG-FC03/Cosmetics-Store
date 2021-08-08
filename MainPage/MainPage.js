d90da869c4994547e3420ac9fa1ce062283838e2
EmanHass <eman_hassouna@hotmail.com>
2021-08-08 22:47:42 +0300

    write code for creating recommended by get data from L.S relates #37

--- MainPage/MainPage.js
+++ MainPage/MainPage.js
@@ -1,4 +1,51 @@
 //Declare variables using dom to access elements
-const head=document.getElementsByClassName('head')[0];
+const head = document.getElementsByClassName("head")[0];
+const recom = document.getElementsByClassName("recom")[0];
 
-head.textContent+=' '+localStorage.getItem('userName');
+head.textContent += " " + localStorage.getItem("userName");
+
+// Create Recommended Section by get data from local Storage.
+setTimeout(() => {
+  const data = JSON.parse(localStorage.getItem(" dataTotal"));
+
+  const headRecom = document.createElement("h2");
+  headRecom.textContent = "Recommended Today";
+  recom.appendChild(headRecom);
+
+  // Loop => to choose 2 random products from data
+  for (let i = 0; i < 2; ++i) {
+    let random = Math.floor(Math.random() * data.length);
+
+    const recomBox = document.createElement("section");
+    const imageBox = document.createElement("div");
+    const recomImg = document.createElement("img");
+    const recomDesc = document.createElement("div");
+    const idProduct = document.createElement("span");
+    const name = document.createElement("h4");
+    const spanName=document.createElement('span');
+    const price = document.createElement("h4");
+
+    recom.appendChild(recomBox);
+    recomBox.appendChild(imageBox);
+    imageBox.appendChild(recomImg);
+    recomBox.appendChild(recomDesc);
+    recomDesc.appendChild(idProduct);
+    recomDesc.appendChild(name);
+    name.appendChild(spanName);
+    recomDesc.appendChild(price);
+
+    recomBox.setAttribute("class", "recomBox");
+    imageBox.setAttribute("class", "imageBox");
+    recomImg.setAttribute("class", "recomImg");
+    recomDesc.setAttribute("class", "recomDesc");
+    idProduct.setAttribute("class", "idProduct");
+    name.setAttribute('class','recomName');
+    recomImg.setAttribute("src", `${data[random].image}`);
+
+    spanName.textContent='Name';
+    idProduct.textContent = `${data[random].id}`;
+    idProduct.style.display = "none";
+    name.textContent = `Name: ${data[random].name}`;
+    price.textContent = `Price: ${data[random].price}`;
+  }
+});
