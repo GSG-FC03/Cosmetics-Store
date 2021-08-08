e3f71ea6b19ec1ffcdb36e07cb6e29b1d644a709
mo7amedeahb97 <gazacityface@gmail.com>
2021-08-08 17:17:11 +0300

    updated the js file and creating the createslider() function related #35

--- MainPage/MainPage.js
+++ MainPage/MainPage.js
@@ -2,12 +2,12 @@
 const head=document.getElementsByClassName('head')[0];
 
 head.textContent+=' '+localStorage.getItem('userName');
+
 var slideIndex = 1;
 showSlides(slideIndex);
 
 // Next/previous controls
 function plusSlides(n) {
-  
   showSlides(slideIndex += n);
 }
 
@@ -19,15 +19,42 @@ function currentSlide(n) {
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
   slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+}
+
+// function for makeing elem and push them into the slide      
+function createSlider(arr){
+  arr.forEach(item => { // loop through the array 
+// create elements and get the parent container using DOM 
+let chiledDiv = document.createElement('div')
+let createImG = document.createElement('img')
+let createPtag = document.createElement('p')
+let createprice = document.createElement('p')
+let createId = document.createElement('span')
+let parentSec = document.getElementById('slider')
+// Add classes to the created elements to manipulate them
+chiledDiv.classList.add('mySlides')
+chiledDiv.classList.add('fade')
+createPtag.classList.add("text")
+createprice.classList.add("text1")
+createImG.setAttribute('class', 'img')
+createId.setAttribute('class', 'numbertext')
+createImG.setAttribute('src', "1.jpg")
+createImG.setAttribute('src', item.image)
+// insert all the elements to the parent section & arrange them
+parentSec.appendChild(chiledDiv);
+chiledDiv.appendChild(createImG);
+chiledDiv.appendChild(createId);
+chiledDiv.appendChild(createPtag);
+chiledDiv.appendChild(createprice);
+// change the slider content using the data from the given array 
+createPtag.textContent= `Name: ${item.name}`
+createprice.textContent=`Price: ${item.price} $`
+createId.textContent=`id: ${item.id}`
+  });
 }
