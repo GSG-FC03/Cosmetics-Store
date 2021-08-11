e526ead0e5ec60318c058037420e400803a5fc7f
EmanHass <eman_hassouna@hotmail.com>
2021-08-11 12:08:09 +0300

    edit code by add id for delete icon relates #74

--- ShoppingCart/ShoppingCart.js
+++ ShoppingCart/ShoppingCart.js
@@ -12,14 +12,14 @@ if (cartTotal.length != 0)
     myCart += `</div>`;
 
     myCart += `<div class="detail">`;
-    myCart += `<span class='productId' style="display:none">id:${element.id}</span>`;
+    myCart += `<span class='productId' style="display:none">${element.id}</span>`;
     myCart += `<h3 id="productName">${element.name}</h3>`;
     myCart += `<p id="quantity">Quantity:
                  <span id='quantityProdcut'>${element.quantity}</span>
                 </p>`;
     myCart += `<p id="priquantityce"><span>Price: </span>${element.price}$</p>`;
     myCart += `<i class="fas fa-plus-circle plussign"></i> <i class="fas fa-minus-circle minussign"></i>
-      <i class="fas fa-times deleteIcon" ></i>`;
+      <i class="fas fa-times deleteIcon" id='deleteIcon'></i>`;
     myCart += `</div>`;
 
     myCart += `</div>`;
@@ -29,3 +29,4 @@ else {
   // if cartTotal empty convert user to MainPage
   allcart.innerHTML = `<h2>You don't have any Products, Please browse products from <a href=../MainPage/MainPage.html>here</a></h2>`;
 }
+
